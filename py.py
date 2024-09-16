cc6dd2d50c1d99f2671949783fda19bf54380e5e
Leoly016 <tanyashadenko93@gmail.com>
2024-09-16 21:31:50 +0500

    Create py.py

---
+++ py.py
@@ -0,0 +1,44 @@
+a = ("hello world!")
+h = ("h")
+e = ("e")
+l = ("l")
+o = ("o")
+
+while h:
+    print(h)
+    print(e)
+    print(l)
+    print(l)
+    print(o)
+    if a:
+        print("he")
+        if a:
+            print("hel")
+            if a:
+                print("hell")
+                if a:
+                    print("hello")
+                    if a:
+                        print("hello w")
+                        if a:
+                            print("hello wo")
+                            if a:
+                                print("hello wor")
+                                if a:
+                                    print("hello worl")
+                                    if a:
+                                        print("hello world")
+                                        if a:
+                                            print("hello world!")
+                                            if a:
+                                                print("End!")
+                                            break
+print(a)
+
+
+
+
+
+
+
+
